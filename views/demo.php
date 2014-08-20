e8081a281301974d1a24edfc8fc55f177959baf8
david <david@localhost>
2014-08-20 12:55:40 +0200

    Fix views error

--- views/demo.php
+++ views/demo.php
@@ -7,12 +7,5 @@
     </head>
     <body>
         <div><?php $this->language->translate('HELLOWORLD'); ?></div>
-        <div>
-            <button id="chicos">chicos</button>
-            <button id="chicas">chicas</button>
-            <button id="todos">todos</button>
-        </div>
-        <div id="name"></div>
-        <div id="surname"></div>
     </body>
 </html>

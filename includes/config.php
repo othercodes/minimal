a125d1cc1f52b70780ae4cf5d5d880f354ea4ffb
David Unay Santisteban <slavepens@gmail.com>
2014-09-09 17:48:06 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,12 +8,13 @@
  */
 class Config {
 
-    public $offline = 0;
-    public $encoding = 'UTF-8';
-    public $language = 'en_US';
-    public $log_path = 'logs';
-    public $cache = 0;
-    public $cache_path = 'cache';
-    public $cachetime = '3600';
-    public $debug = 0;
+    public $offline     = 0;
+    public $encoding    = 'UTF-8';
+    public $language    = 'es_ES';
+    public $log_path    = 'logs';
+    public $cache       = 0;
+    public $cache_path  = 'cache';
+    public $cache_time  = '3600';
+    public $debug       = 0;
+    public $compress    = 0;
 }

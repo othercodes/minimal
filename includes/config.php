5ff0565005436a3bc081053b43849d93fa08491c
slavepens <slavepens@gmail.com>
2014-09-19 18:26:45 +0200

    Fixed some bad inital config

--- includes/config.php
+++ includes/config.php
@@ -10,7 +10,7 @@ class Config {
 
     public $offline     = 0;
     public $encoding    = 'UTF-8';
-    public $language    = 'es_ES';
+    public $language    = 'en_US';
     public $log_path    = 'logs';
     public $cache       = 0;
     public $cache_path  = 'cache';
